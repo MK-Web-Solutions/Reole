3797ba45cb60bfcf694f59e0c9f659ff93a51eed
mai k <mai_mohsen02@hotmail.com>
2026-02-28 19:34:23 +0200

    typo

--- Reole/src/App.tsx
+++ Reole/src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import AppRoutes from "./router";
 import ScrollToTop from "./components/ScrollToTop";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <BrowserRouter>
