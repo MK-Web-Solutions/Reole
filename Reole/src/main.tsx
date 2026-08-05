1c0c50ac27bd779b6f952d1853c50d21d8e7f001
mai k <mai_mohsen02@hotmail.com>
2026-08-05 01:57:26 +0100

    google analytica

--- Reole/src/main.tsx
+++ Reole/src/main.tsx
@@ -4,6 +4,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Google Analytics
+const GA_ID = 'G-7FF49YQEZ1'
+
+const script = document.createElement('script')
+script.async = true
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+document.head.appendChild(script)
+
+window.dataLayer = window.dataLayer || []
+
+function gtag(...args: any[]) {
+  window.dataLayer.push(args)
+}
+
+gtag('js', new Date())
+gtag('config', GA_ID)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
